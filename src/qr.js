ae6ba031c6ba7ed0ae9ab162b1b069bd0963188d
Juani <juan.canete@prosoftwarefactory.com.ar>
2022-11-07 11:11:30 -0300

    adding some funny delay

--- src/qr.js
+++ src/qr.js
@@ -4,6 +4,7 @@
   const path = require("path");
   const background = fs.readFileSync(path.resolve(__dirname, '.././assets/backgrounds/background.png'));
   const outputDir = 'output/'
+  const delay = 1000;
 
   const buffer = await new AwesomeQR({
     text: 'https://www.linkedin.com/in/juanignacioca%C3%B1ete/',
@@ -12,18 +13,27 @@
   }).draw();
 
   if(!fs.existsSync(outputDir)) {
-    console.log('There is not an ouput folder, creating directory...');
+    console.info(`>>>>> There is not '${outputDir}' folder, creating directory...`);
+    await new Promise(resolve => setTimeout(resolve, delay));
     try {
       fs.mkdirSync(outputDir);
     } catch (error) {
-      throw new Error(`An error just happened creating ${outputDir}.\nHere is the error: \n\t\t${error}`)
+      throw new Error(`An error just happened creating '${outputDir}'.\nHere is the error: \n\t\t${error}`)
+    } finally {
+      console.info(`>>>>> '${outputDir}' created succesfully!`);
+      await new Promise(resolve => setTimeout(resolve, delay));
     };
   };
 
   try {
+    console.info('>>>>> Generating QR code...');
+    await new Promise(resolve => setTimeout(resolve, delay));
     fs.writeFileSync('output/qr.png', buffer);
   } catch (error) {
     throw new Error(`An error just happened creating QR code.\nHere is the error: \n\t\t${error}`)
+  } finally {
+    console.info('>>>>> Alright, QR code generated succesfully!');
+    await new Promise(resolve => setTimeout(resolve, delay));
   };
 
 })();
