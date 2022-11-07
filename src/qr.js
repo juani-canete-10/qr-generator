f47f26eeb2401d16958f5ebed01da6cb4763d597
Juani <juan.canete@prosoftwarefactory.com.ar>
2022-11-07 11:00:45 -0300

    adding error handling

--- src/qr.js
+++ src/qr.js
@@ -3,6 +3,7 @@
   const fs = require("fs");
   const path = require("path");
   const background = fs.readFileSync(path.resolve(__dirname, '.././assets/backgrounds/background.png'));
+  const outputDir = 'output/'
 
   const buffer = await new AwesomeQR({
     text: 'https://www.linkedin.com/in/juanignacioca%C3%B1ete/',
@@ -10,5 +11,19 @@
     backgroundImage: background,
   }).draw();
 
-  fs.writeFileSync('output/qr.png', buffer);
+  if(!fs.existsSync(outputDir)) {
+    console.log('There is not an ouput folder, creating directory...');
+    try {
+      fs.mkdirSync(outputDir);
+    } catch (error) {
+      throw new Error(`An error just happened creating ${outputDir}.\nHere is the error: \n\t\t${error}`)
+    };
+  };
+
+  try {
+    fs.writeFileSync('output/qr.png', buffer);
+  } catch (error) {
+    throw new Error(`An error just happened creating QR code.\nHere is the error: \n\t\t${error}`)
+  };
+
 })();
